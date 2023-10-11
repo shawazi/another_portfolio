48567933e381df2554ecec00e4145165fd274c4f
“shawazi” <shawazisonfire@gmail.com>
2023-10-11 11:57:03 -0400

    basic packages installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Typography, Button, Container } from '@mui/material';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main>
+      <Container display="flex">
+        <Typography align="center">Text</Typography>
+        <div>
+          <Typography variant="h4" color="secondary">
+            Heading with Primary Color
+          </Typography>
+          <Typography variant="body1" color="secondary.light">
+            Sample text with Primary Text Color
+          </Typography>
+          <Button variant="contained" color="secondary">
+            Secondary Color Button
+          </Button>          <Button variant="contained" color="secondary.light">
+            Secondary Color Button
+          </Button>          <Button variant="contained" color="secondary">
+            Secondary Color Button
+          </Button>
+        </div>
+      </Container>
+    </main>
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeProvider, createTheme } from '@mui/material'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+let theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1f264e',
+      light: '#5167d8',
+      dark: '#171731',
+    },
+    secondary: {
+      main: '#2f8a80',
+      dark: '#06231f',
+    },
+    text: {
+      primary: '#bdbdbd',
+      secondary: '#b0bec5',
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
